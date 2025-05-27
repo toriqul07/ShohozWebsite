2882ca2202afc88005423d8540a6696f56fcdb8e
toriqul07 <torikul170074@gmail.com>
2025-05-27 19:51:29 +0600

    Third commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { MiddlePartComponent } from '../middle-part/middle-part.component';
 import { FooterComponent } from '../footer/footer.component';
+import { OrderPartComponent } from '../order-part/order-part.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,MiddlePartComponent,FooterComponent],
+  imports: [NavbarComponent,MiddlePartComponent,FooterComponent,OrderPartComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
